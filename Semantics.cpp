47084eb2e103e4af4a8dcb04c5aaae2331c750fc
danibondar <danibondar@gmail.com>
2021-12-23 07:16:40 +0200

    code reformat

--- Semantics.cpp
+++ Semantics.cpp
@@ -7,138 +7,138 @@ bool mainExits = false;
 int lineno = 0;
 
 symbolRow::symbolRow(string name,
-                     int pos,
-                     vector<string> types,
-                     bool is_const,
-                     vector<bool> constFormals,
-                     bool isFunc)
-        : name(name),
-          pos(pos),
-          types(types),
-          is_const(is_const),
-          constFormals(constFormals),
-          isFunc(isFunc) {}
+					 int pos,
+					 vector<string> types,
+					 bool is_const,
+					 vector<bool> constFormals,
+					 bool isFunc)
+	: name(name),
+	  pos(pos),
+	  types(types),
+	  is_const(is_const),
+	  constFormals(constFormals),
+	  isFunc(isFunc) {}
 
 bool symbolRow::operator==(symbolRow &other) {
-    return (this->name == other.name);// && (this->types == other.types);
+	return (this->name == other.name);// && (this->types == other.types);
 }
 
-SymbolTable::SymbolTable() : symbolTable({}), isWhileScope(false){}
+SymbolTable::SymbolTable() : symbolTable({}), isWhileScope(false) {}
 SymbolTable::SymbolTable(SymbolTable *other) : symbolTable(other->symbolTable), isWhileScope(other->isWhileScope) {
 }
 
 bool SymbolTable::contains(string id, vector<string> type) {
-    auto dummyConst = false;
-    symbolRow dummy(id, -1, type, dummyConst, {});
-    bool res = false;
-    vector<symbolRow>::iterator it = this->symbolTable.begin();
-    for (it; it != this->symbolTable.end(); it++) {
-        res = (*it == dummy);
-        if (res) {
-            return res;
-        }
-    }
-    return res;
+	auto dummyConst = false;
+	symbolRow dummy(id, -1, type, dummyConst, {});
+	bool res = false;
+	vector<symbolRow>::iterator it = this->symbolTable.begin();
+	for (it; it != this->symbolTable.end(); it++) {
+		res = (*it == dummy);
+		if (res) {
+			return res;
+		}
+	}
+	return res;
 }
 
 void m_glob() {
-    SymbolTable new_scope;
-    vector<string> print_vec = {"VOID", "STRING"};
-    symbolRow print_our("print", 0, print_vec, false, {});
-    vector<string> printi_vec = {"VOID", "INT"};
-    symbolRow printi_our("printi", 0, printi_vec, false, {});
-    new_scope.symbolTable.push_back(print_our);
-    new_scope.symbolTable.push_back(printi_our);
-    globSymTable.push_back(new_scope);
-    offsetStack.push_back(0);
-    return;
+	SymbolTable new_scope;
+	vector<string> print_vec = {"VOID", "STRING"};
+	symbolRow print_our("print", 0, print_vec, false, {});
+	vector<string> printi_vec = {"VOID", "INT"};
+	symbolRow printi_our("printi", 0, printi_vec, false, {});
+	new_scope.symbolTable.push_back(print_our);
+	new_scope.symbolTable.push_back(printi_our);
+	globSymTable.push_back(new_scope);
+	offsetStack.push_back(0);
+	return;
 }
 
 void m_newScope() {
-    //Create and insert new scope table
-    SymbolTable new_scope;
-    globSymTable.push_back(new_scope);
-    //Copy the last offset and insert as new offset on top of the stack
-    offsetStack.push_back(*offsetStack.end());
+	//Create and insert new scope table
+	SymbolTable new_scope;
+	globSymTable.push_back(new_scope);
+	//Copy the last offset and insert as new offset on top of the stack
+	offsetStack.push_back(*offsetStack.end());
 }
 
 void m_newScopeWhile() {
-    //Create and insert new scope table
-    SymbolTable new_scope;
-    new_scope.isWhileScope = true;
-    globSymTable.push_back(new_scope);
-    //Copy the last offset and insert as new offset on top of the stack
-    offsetStack.push_back(*offsetStack.end());
+	//Create and insert new scope table
+	SymbolTable new_scope;
+	new_scope.isWhileScope = true;
+	globSymTable.push_back(new_scope);
+	//Copy the last offset and insert as new offset on top of the stack
+	offsetStack.push_back(*offsetStack.end());
 }
 
 void m_endScope() {
-    end_scope();
+	end_scope();
 }
 
 void end_scope() {
-    output::endScope();
-    SymbolTable table = globSymTable.pop_back();
-    int offset = offsetStack.pop_back();
-    for (auto it: table.symbolTable) {
-        if (it.types.size() == 1) {
-            //ID
-            output::printID(it.name, it.pos, it.types[0]);
-        } else {
-            //FUNCTION
-            string retVal = it.types[0];
-            it.types.erase(it.types.begin());
-            string funcType = output::makeFunctionType(retVal, it.types);
-            output::printID(it.name, 0, funcType);
-        }
-    }
+	output::endScope();
+	SymbolTable table = globSymTable.pop_back();
+	int offset = offsetStack.pop_back();
+	for (auto it: table.symbolTable) {
+		if (it.types.size() == 1) {
+			//ID
+			output::printID(it.name, it.pos, it.types[0]);
+		} else {
+			//FUNCTION
+			string retVal = it.types[0];
+			it.types.erase(it.types.begin());
+			string funcType = output::makeFunctionType(retVal, it.types);
+			output::printID(it.name, 0, funcType);
+		}
+	}
 }
 
 symbolRow findSymbolRow(string id) {
-    symbolRow res = symbolRow("", -1, {""}, false, {false});
-    for (auto itGlob: globSymTable) {
-        for (auto itScope: itGlob.symbolTable) {
-            if (itScope.name == id) {
-                res = itScope;
-                return res;
-            }
-        }
-    }
-    return res;
+	symbolRow res = symbolRow("", -1, {""}, false, {false});
+	for (auto itGlob: globSymTable) {
+		for (auto itScope: itGlob.symbolTable) {
+			if (itScope.name == id) {
+				res = itScope;
+				return res;
+			}
+		}
+	}
+	return res;
 }
 
 bool isIdentifierExists(string id) {
-    bool res = false;
-    symbolRow check = findSymbolRow(id);
-    if (check.name == id) {
-        res = true;
-    }
-    return res;
+	bool res = false;
+	symbolRow check = findSymbolRow(id);
+	if (check.name == id) {
+		res = true;
+	}
+	return res;
 }
 
 vector<string> findIdentifierType(string id) {
-    vector<string> types = {};
-    symbolRow identifier = findSymbolRow(id);
-    if (!identifier.name.empty()) {
-        types = identifier.types;
-    }
-    return types;
+	vector<string> types = {};
+	symbolRow identifier = findSymbolRow(id);
+	if (!identifier.name.empty()) {
+		types = identifier.types;
+	}
+	return types;
 }
 
 bool isIdentifierConst(string id) {
-    bool res = false;
-    symbolRow check = findSymbolRow(id);
-    if (check.name == id) {
-        res = check.is_const;
-    }
-    return res;
+	bool res = false;
+	symbolRow check = findSymbolRow(id);
+	if (check.name == id) {
+		res = check.is_const;
+	}
+	return res;
 }
 //////////////////////////////////////////////////
 
 program::program() : Node("program") {
-    void end_scope();
-    if (!mainExits) {
-        output::errorMainMissing();
-    }
+	void end_scope();
+	if (!mainExits) {
+		output::errorMainMissing();
+	}
 }
 
 funcs::funcs() : Node("funcs") {
@@ -146,234 +146,237 @@ funcs::funcs() : Node("funcs") {
 }
 
 funcsDecl::funcsDecl(retType &retType, string id, formals &formals, statements &statements) : Node("funcsDecl") {
-    if (id == "main") {
-        if (mainExits) {
-            output::errorDef(lineno, id);
-            exit(0);
-        }
-        mainExits = true;
-    }
-    if (isIdentifierExists(id)) {
-        output::errorDef(lineno, id);
-        exit(0);
-    }
+	if (id == "main") {
+		if (mainExits) {
+			output::errorDef(lineno, id);
+			exit(0);
+		}
+		mainExits = true;
+	}
+	if (isIdentifierExists(id)) {
+		output::errorDef(lineno, id);
+		exit(0);
+	}
 
-    vector<string> funcTypes;
-    vector<bool> funcConstTypes;
-    funcTypes.push_back(retType.typeName);
-    for (auto formal: formals.formalsVector) {
-        funcTypes.push_back(formal.typeName);
-        funcConstTypes.push_back(formal.isConst);
-    }
-    symbolRow symbol_row(id, 0, funcTypes, false, funcConstTypes, true);
-    globSymTable.end()->symbolTable.push_back(symbol_row);
-    end_scope();
+	vector<string> funcTypes;
+	vector<bool> funcConstTypes;
+	funcTypes.push_back(retType.typeName);
+	for (auto formal: formals.formalsVector) {
+		funcTypes.push_back(formal.typeName);
+		funcConstTypes.push_back(formal.isConst);
+	}
+	symbolRow symbol_row(id, 0, funcTypes, false, funcConstTypes, true);
+	globSymTable.end()->symbolTable.push_back(symbol_row);
+	end_scope();
 }
 
 retType::retType(type &type) : Node("retType") {
-    this->typeName = type.typeName;
+	this->typeName = type.typeName;
 }
 
 retType::retType(string typeName) : Node("retType") {
-    if (typeName != "VOID") {
-        output::errorSyn(lineno);
-        exit(0);
-    }
-    this.typeName = typeName;
+	if (typeName != "VOID") {
+		output::errorSyn(lineno);
+		exit(0);
+	}
+	this.typeName = typeName;
 }
 
 formals::formals() : Node("formals") {
-    this->formalsVector = {};
+	this->formalsVector = {};
 }
 
 formals::formals(formalsList &formals) : Node("formals") {
-    for (auto formal: formals.formalsVector) {
-        for (auto nextFormal: formals.formalsVector) {
-            if (formal == nextFormal) {
-                continue;
-            }
-            if (formal.id == nextFormal.id) {
-                output::errorDef(lineno, formal.id);
-                exit(0);
-            }
-        }
-    }
-    int i = -1;
-    for (auto it: formals.formalsVector) {
-        symbolRow formal(it.id, i, {it.typeName}, true, {}, false);
-        globSymTable.end()->symbolTable.push_back(formal);
-        i--;
-    }
-    this->formalsVector = formals.formalsVector;
+	for (auto formal: formals.formalsVector) {
+		for (auto nextFormal: formals.formalsVector) {
+			if (formal == nextFormal) {
+				continue;
+			}
+			if (formal.id == nextFormal.id) {
+				output::errorDef(lineno, formal.id);
+				exit(0);
+			}
+		}
+	}
+	int i = -1;
+	for (auto it: formals.formalsVector) {
+		symbolRow formal(it.id, i, {it.typeName}, true, {}, false);
+		globSymTable.end()->symbolTable.push_back(formal);
+		i--;
+	}
+	this->formalsVector = formals.formalsVector;
 }
 
 formalsList::formalsList(formalsDecl &formalsDecl) : Node("formalsVector") {
-    this->formalsVector.push_back(formalsVector);
+	this->formalsVector.push_back(formalsVector);
 }
 
 formalsList::formalsList(formalsDecl &formalsDecl, formalsList &formalsList) : Node("formalsVector") {
-    this->formalsVector.push_back(formalsDecl);
-    this->formalsVector.insert(this->formalsVector.end(), formalsList.formalsVector.begin(), formalsList.formalsVector.end());
+	this->formalsVector.push_back(formalsDecl);
+	this->formalsVector.insert(this->formalsVector.end(),
+							   formalsList.formalsVector.begin(),
+							   formalsList.formalsVector.end());
 }
 
 formalsDecl::formalsDecl(typeAnnotation &typeAnnotation, type &type, string id) : Node("formalsDecl") {
-    this->id = id;
-    this->typeName = type.typeName;
-    if(typeAnnotation.annoType == "CONST"){
-      this->isConst = true;
-    }else{
-      this->isConst = false;
-    }
+	this->id = id;
+	this->typeName = type.typeName;
+	if (typeAnnotation.annoType == "CONST") {
+		this->isConst = true;
+	} else {
+		this->isConst = false;
+	}
 }
 
 statements::statements(statement &statement) : Node("statements") {
-    this->vecStatements.push_back(statement);
+	this->vecStatements.push_back(statement);
 }
 
 statements::statements(statements &statements, statement &statement) : Node("statements") {
-    this->vecStatements = statements.vecStatements;
-    this->vecStatements.push_back(statement);
+	this->vecStatements = statements.vecStatements;
+	this->vecStatements.push_back(statement);
 }
 
 statement::statement(OpenStatement &OpenStatement) : Node("statement") {
-    //todo:: nothing?
+	//todo:: nothing?
 }
 
 statement::statement(ClosedStatement &ClosedStatement) : Node("statement") {
-    //todo:: nothing?
+	//todo:: nothing?
 }
 
 OpenStatement::OpenStatement(string keyWord, exp &exp, statement &statement) : Node("OpenStatement") {
-    if (keyWord != "IF") {
-        output::errorSyn(lineno);
-        exit(0);
-    }
-    if (exp.typeName != "BOOL") {
-        output::errorMismatch(lineno);
-        exit(0);
-    }
-    end_scope();
+	if (keyWord != "IF") {
+		output::errorSyn(lineno);
+		exit(0);
+	}
+	if (exp.typeName != "BOOL") {
+		output::errorMismatch(lineno);
+		exit(0);
+	}
+	end_scope();
 }
 
 OpenStatement::OpenStatement(string firstKeyWord,
-                             exp &exp,
-                             ClosedStatement &ClosedStatement,
-                             string secondKeyWord,
-                             OpenStatement &OpenStatement) : Node("OpenStatement") {
-    if (firstKeyWord != "IF" || secondKeyWord != "ELSE") {
-        output::errorSyn(lineno);
-        exit(0);
-    }
-    if (exp.typeName != "BOOL") {
-        output::errorMismatch(lineno);
-        exit(0);
-    }
-    end_scope();
+							 exp &exp,
+							 ClosedStatement &ClosedStatement,
+							 string secondKeyWord,
+							 OpenStatement &OpenStatement) : Node("OpenStatement") {
+	if (firstKeyWord != "IF" || secondKeyWord != "ELSE") {
+		output::errorSyn(lineno);
+		exit(0);
+	}
+	if (exp.typeName != "BOOL") {
+		output::errorMismatch(lineno);
+		exit(0);
+	}
+	end_scope();
 }
 
 OpenStatement::OpenStatement(string firstKeyWord, exp &exp, OpenStatement &OpenStatement) : Node("OpenStatement") {
-    if (firstKeyWord != "WHILE") {
-        output::errorSyn(lineno);
-        exit(0);
-    }
-    if (exp.typeName != "BOOL") {
-        output::errorMismatch(lineno);
-        exit(0);
-    }
-    end_scope();
+	if (firstKeyWord != "WHILE") {
+		output::errorSyn(lineno);
+		exit(0);
+	}
+	if (exp.typeName != "BOOL") {
+		output::errorMismatch(lineno);
+		exit(0);
+	}
+	end_scope();
 }
 
 ClosedStatement::ClosedStatement(SimpleStatement &SimpleStatement) : Node("ClosedStatement") {
-    //todo:: nothing?
+	//todo:: nothing?
 }
 
 ClosedStatement::ClosedStatement(string firstKeyWord, exp &exp, ClosedStatement &ClosedStatement,
-                                 string secondKeyWord, OpenStatement &OpenStatement) : Node("ClosedStatement") {
-    if (firstKeyWord != "IF" || secondKeyWord != "ELSE") {
-        output::errorSyn(lineno);
-        exit(0);
-    }
-    if (exp.typeName != "BOOL") {
-        output::errorMismatch(lineno);
-        exit(0);
-    }
-    end_scope();
+								 string secondKeyWord, OpenStatement &OpenStatement) : Node("ClosedStatement") {
+	if (firstKeyWord != "IF" || secondKeyWord != "ELSE") {
+		output::errorSyn(lineno);
+		exit(0);
+	}
+	if (exp.typeName != "BOOL") {
+		output::errorMismatch(lineno);
+		exit(0);
+	}
+	end_scope();
 }
 
-ClosedStatement::ClosedStatement(string firstKeyWord, exp &exp, ClosedStatement &ClosedStatement) : Node("ClosedStatement") {
-    if (firstKeyWord != "WHILE") {
-        output::errorSyn(lineno);
-        exit(0);
-    }
-    if (exp.typeName != "BOOL") {
-        output::errorMismatch(lineno);
-        exit(0);
-    }
-    end_scope();
+ClosedStatement::ClosedStatement(string firstKeyWord, exp &exp, ClosedStatement &ClosedStatement) : Node(
+	"ClosedStatement") {
+	if (firstKeyWord != "WHILE") {
+		output::errorSyn(lineno);
+		exit(0);
+	}
+	if (exp.typeName != "BOOL") {
+		output::errorMismatch(lineno);
+		exit(0);
+	}
+	end_scope();
 }
 
 SimpleStatement::SimpleStatement(string cmd) : Node("SimpleStatement") {
-  switch (cmd) { //TODO - switch doesn't work on string
-        case "RETURN":break;
-        case "BREAK":break;
-        case "CONTINUE":break;
-    }
+	switch (cmd) { //TODO - switch doesn't work on string
+		case "RETURN":break;
+		case "BREAK":break;
+		case "CONTINUE":break;
+	}
 } //return VOID, break, continue
 
 SimpleStatement::SimpleStatement(statements &statements) : Node("SimpleStatement") {
-    end_scope();
+	end_scope();
 }    //LBRACE m_newScope statements RBRACE
 
 SimpleStatement::SimpleStatement(typeAnnotation &typeAnnotation, type &type, string id) : Node("SimpleStatement") {
-    if (isIdentifierExists(id)) {
-        output::errorDef(lineno, id);
-        exit(0);
-    }
-    if (typeAnnotation.isConst) {
-        output::errorConstDef(lineno);
-        exit(0);
-    }
-    int pos = *offsetStack.end() + 1;
-    *offsetStack.end() = pos;
-    symbolRow newIdentifier(id, pos, {type.typeName}, typeAnnotation.isConst, {}, false);
-    globSymTable.end()->symbolTable.push_back(newIdentifier);
+	if (isIdentifierExists(id)) {
+		output::errorDef(lineno, id);
+		exit(0);
+	}
+	if (typeAnnotation.isConst) {
+		output::errorConstDef(lineno);
+		exit(0);
+	}
+	int pos = *offsetStack.end() + 1;
+	*offsetStack.end() = pos;
+	symbolRow newIdentifier(id, pos, {type.typeName}, typeAnnotation.isConst, {}, false);
+	globSymTable.end()->symbolTable.push_back(newIdentifier);
 } //typeAnnotation typeName ID SC
 
 SimpleStatement::SimpleStatement(typeAnnotation &typeAnnotation, type &type, string id, exp &exp) : Node(
-        "SimpleStatement") {
-    if (isIdentifierExists(id)) {
-        output::errorDef(lineno, id);
-        exit(0);
-    }
-    if (type.typeName != exp.typeName) {
-        output::errorMismatch(lineno);
-        exit(0);
-    }
-    int pos = *offsetStack.end() + 1;
-    *offsetStack.end() = pos;
-    symbolRow newIdentifier(id, pos, {type.typeName}, typeAnnotation.isConst, {}, false);
-    globSymTable.end()->symbolTable.push_back(newIdentifier);
+	"SimpleStatement") {
+	if (isIdentifierExists(id)) {
+		output::errorDef(lineno, id);
+		exit(0);
+	}
+	if (type.typeName != exp.typeName) {
+		output::errorMismatch(lineno);
+		exit(0);
+	}
+	int pos = *offsetStack.end() + 1;
+	*offsetStack.end() = pos;
+	symbolRow newIdentifier(id, pos, {type.typeName}, typeAnnotation.isConst, {}, false);
+	globSymTable.end()->symbolTable.push_back(newIdentifier);
 }    //typeAnnotation typeName ID ASSIGN exp SC
 
 SimpleStatement::SimpleStatement(string id, string assign, exp &exp) : Node("SimpleStatement") {
-    if (!isIdentifierExists(id)) {
-        output::errorUndef(lineno, id);
-        exit(0);
-    }
-    //TODO - I dont understand what you try to do because type is not string but vector<string>
-    string idType = findIdentifierType(id);
-    if (idType != exp.typeName) {
-        output::errorMismatch(lineno);
-        exit(0);
-    }
-    if (isIdentifierConst(id)) {
-        output::errorConstMismatch(lineno);
-        exit(0);
-    }
+	if (!isIdentifierExists(id)) {
+		output::errorUndef(lineno, id);
+		exit(0);
+	}
+	//TODO - I dont understand what you try to do because type is not string but vector<string>
+	string idType = findIdentifierType(id);
+	if (idType != exp.typeName) {
+		output::errorMismatch(lineno);
+		exit(0);
+	}
+	if (isIdentifierConst(id)) {
+		output::errorConstMismatch(lineno);
+		exit(0);
+	}
 }//ID ASSIGN exp SC
 
 SimpleStatement::SimpleStatement(call &call) : Node("SimpleStatement") {
-    //todo:: nothing?
+	//todo:: nothing?
 }
 
 SimpleStatement::SimpleStatement(exp &exp) : Node("SimpleStatement") {
@@ -381,20 +384,20 @@ SimpleStatement::SimpleStatement(exp &exp) : Node("SimpleStatement") {
 }//RETURN exp SC
 
 type::type(string
-           typeName) {
-    if (typeName == "INT") {
-        typeName = "INT";
-    } else if (typeName == "BYTE") {
-        typeName = "BYTE";
-    } else if (typeName == "BOOL") {
-        typeName = "BOOL";
-    } else {
+		   typeName) {
+	if (typeName == "INT") {
+		typeName = "INT";
+	} else if (typeName == "BYTE") {
+		typeName = "BYTE";
+	} else if (typeName == "BOOL") {
+		typeName = "BOOL";
+	} else {
 //TODO: print desired error
-    }
+	}
 }
 
 typeAnnotation::typeAnnotation(string
-                               annoType) :
-        annoType(annoType) {};
+							   annoType) :
+	annoType(annoType) {};
 
 
--- Semantics.h
+++ Semantics.h
@@ -27,40 +27,40 @@ class exp;
 extern vector<SymbolTable> globSymTable;
 extern vector<int> offsetStack;
 extern bool mainExits;
-extern  int lineno;
+extern int lineno;
 #define YYSTYPE Node*
 
 class Node {
- public:
-  string val;
-  Node() : val("") {};
-  Node(string val) : val(val) {};
+  public:
+	string val;
+	Node() : val("") {};
+	Node(string val) : val(val) {};
 };
 
 class symbolRow {
- public:
-  string name;
-  int pos;
-  vector<string> types;
-  bool is_const;
-  vector<bool> constFormals;
-  bool isFunc = false;
-  symbolRow(string name,
-            int pos,
-            vector<string> types,
-            bool is_const,
-            vector<bool> constFormals,
-            bool isFunc = false);
-  bool operator==(symbolRow &other);
+  public:
+	string name;
+	int pos;
+	vector<string> types;
+	bool is_const;
+	vector<bool> constFormals;
+	bool isFunc = false;
+	symbolRow(string name,
+			  int pos,
+			  vector<string> types,
+			  bool is_const,
+			  vector<bool> constFormals,
+			  bool isFunc = false);
+	bool operator==(symbolRow &other);
 };
 
 class SymbolTable {
- public:
-  vector<symbolRow> symbolTable;
-  bool isWhileScope;
-  SymbolTable();
-  SymbolTable(SymbolTable *other);
-  bool contains(string id, vector<string> type);
+  public:
+	vector<symbolRow> symbolTable;
+	bool isWhileScope;
+	SymbolTable();
+	SymbolTable(SymbolTable *other);
+	bool contains(string id, vector<string> type);
 };
 
 void m_glob();
@@ -81,132 +81,134 @@ bool isIdentifierConst(string id);
 //////////////////////////////////////////////////
 
 class program : Node {
- public:
-  program();
+  public:
+	program();
 };
 
 class funcs : Node {
- public:
-  funcs();
+  public:
+	funcs();
 };
 
 class funcsDecl : Node {
- public:
-  funcsDecl(retType &retType, string id, formals &formals, statements &statements);
+  public:
+	funcsDecl(retType &retType, string id, formals &formals, statements &statements);
 };
 
 class retType : Node {
- public:
-  string typeName;
-  retType(type &type);
-  retType(string typeName);
+  public:
+	string typeName;
+	retType(type &type);
+	retType(string typeName);
 };
 
 class formals : Node {
- public:
-  vector<formalsDecl> formalsVector;
-  formals();
-  formals(formalsList &formalsList);
+  public:
+	vector<formalsDecl> formalsVector;
+	formals();
+	formals(formalsList &formalsList);
 };
 
 class formalsList : Node {
- public:
-  vector<formalsDecl> formalsVector;
-  formalsList(formalsDecl &formalsDecl);
-  formalsList(formalsDecl &formalsDecl, formalsList &formalsList);
+  public:
+	vector<formalsDecl> formalsVector;
+	formalsList(formalsDecl &formalsDecl);
+	formalsList(formalsDecl &formalsDecl, formalsList &formalsList);
 };
 
 class formalsDecl : Node {
- public:
-  string typeName;
-  bool isConst;
-  string id;
-  formalsDecl(typeAnnotation &typeAnnotation, type &type, string id);
+  public:
+	string typeName;
+	bool isConst;
+	string id;
+	formalsDecl(typeAnnotation &typeAnnotation, type &type, string id);
 };
 
 class statements : Node {
- public:
-  vector<statement> vecStatements;
-  statements(statement &statement);
-  statements(statements &statements, statement &statement);
+  public:
+	vector<statement> vecStatements;
+	statements(statement &statement);
+	statements(statements &statements, statement &statement);
 };
 
 class statement : Node {
- public:
-  statement(OpenStatement &OpenStatement);
-  statement(ClosedStatement &ClosedStatement);
+  public:
+	statement(OpenStatement &OpenStatement);
+	statement(ClosedStatement &ClosedStatement);
 };
 
 class OpenStatement : Node {
- public:
-  OpenStatement(string keyWord, exp &exp, statement &statement);
-  OpenStatement(string firstKeyWord,
-                exp &exp,
-                ClosedStatement &ClosedStatement,
-                string secondKeyWord,
-                OpenStatement &OpenStatement);
-  OpenStatement(string keyWord, exp &exp, OpenStatement &OpenStatement);
+  public:
+	OpenStatement(string keyWord, exp &exp, statement &statement);
+	OpenStatement(string firstKeyWord,
+				  exp &exp,
+				  ClosedStatement &ClosedStatement,
+				  string secondKeyWord,
+				  OpenStatement &OpenStatement);
+	OpenStatement(string keyWord, exp &exp, OpenStatement &OpenStatement);
 };
 
 class ClosedStatement : Node {
- public:
-  ClosedStatement(SimpleStatement &SimpleStatement);
-  ClosedStatement(string firstKeyWord,
-                  exp &exp,
-                  ClosedStatement &ClosedStatement,
-                  string secondKeyWord,
-                  OpenStatement &OpenStatement);
-  ClosedStatement(string keyWord, exp &exp, ClosedStatement &ClosedStatement);
+  public:
+	ClosedStatement(SimpleStatement &SimpleStatement);
+	ClosedStatement(string firstKeyWord,
+					exp &exp,
+					ClosedStatement &ClosedStatement,
+					string secondKeyWord,
+					OpenStatement &OpenStatement);
+	ClosedStatement(string keyWord, exp &exp, ClosedStatement &ClosedStatement);
 };
 
 class SimpleStatement : Node {
- public:
-  SimpleStatement(string cmd); //return, break, continue
-  SimpleStatement(statements &statements);
-  SimpleStatement(typeAnnotation &typeAnnotation, type &type, string id);
-  SimpleStatement(typeAnnotation &typeAnnotation, type &type, string id, exp &exp);
-  SimpleStatement(string id, string assign = "ASSIGN", exp &exp); //TODO - You can't define parameter without default after one with default
-  SimpleStatement(call &call);
-  SimpleStatement(exp &exp);
+  public:
+	SimpleStatement(string cmd); //return, break, continue
+	SimpleStatement(statements &statements);
+	SimpleStatement(typeAnnotation &typeAnnotation, type &type, string id);
+	SimpleStatement(typeAnnotation &typeAnnotation, type &type, string id, exp &exp);
+	SimpleStatement(string id,
+					string assign = "ASSIGN",
+					exp &exp); //TODO - You can't define parameter without default after one with default
+	SimpleStatement(call &call);
+	SimpleStatement(exp &exp);
 
 };
 
 class call : Node {
- public:
-  call(string id, expList &expList);
-  call(string id);
+  public:
+	call(string id, expList &expList);
+	call(string id);
 };
 
 class expList : Node {
- public:
-  expList(exp &exp);
-  expList(exp &exp, expList &expList);
+  public:
+	expList(exp &exp);
+	expList(exp &exp, expList &expList);
 };
 
 class type : Node {
- public:
-  string typeName;
-  type(string typeName);
+  public:
+	string typeName;
+	type(string typeName);
 };
 
 class typeAnnotation : Node {
- public:
-  string annoType;
-  bool isConst;
-  typeAnnotation(string annoType = "");
+  public:
+	string annoType;
+	bool isConst;
+	typeAnnotation(string annoType = "");
 };
 
 class exp : Node {
- public:
-  string typeName;
-  exp(exp &exp);
-  exp(exp &firstExp, string op, exp &secExp);
-  exp(string id);
-  exp(call &call);
-  exp(int val, bool isB = false);
-  exp(bool val);
-  exp(string op, exp &exp);
-  exp(typeAnnotation &typeAnnotation, type &type, exp &exp);
+  public:
+	string typeName;
+	exp(exp &exp);
+	exp(exp &firstExp, string op, exp &secExp);
+	exp(string id);
+	exp(call &call);
+	exp(int val, bool isB = false);
+	exp(bool val);
+	exp(string op, exp &exp);
+	exp(typeAnnotation &typeAnnotation, type &type, exp &exp);
 };
 
 #endif //COMPIWET3__SEMANTICS_H_
